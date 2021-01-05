fa8107cf9d417d399cc1706149bebe2aaf5201c6
Nakyung Im <nakyoung99@gmail.com>
2021-01-05 16:31:28 +0900

    날씨정보요청 with axios-check console

--- noteapp-react-redux-antd/src/App.js
+++ noteapp-react-redux-antd/src/App.js
@@ -1,19 +1,28 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import "antd/dist/antd.css";
+import axios from "axios"; // 추가
 
 // import NoteListContainer from "./containers/ClassContainer";
 import NoteLayoutContainer from "./containers/FunctionalContainer";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        {/* <NoteListContainer /> */}
-        <NoteLayoutContainer />
-      </div>
-    );
-  }
+const App = () => {
+  useEffect(() => { // 추가
+    axios
+    .get(
+      "http://api.openweathermap.org/data/2.5/weather?q=Seoul&?units=metric&APPID=46ffea12534e8bf301ad51176ab9e517"
+    )
+    .then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {/* <NoteListContainer /> */}
+      <NoteLayoutContainer />
+    </div>
+  );
 }
 
 export default App;
