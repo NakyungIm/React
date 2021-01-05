da2c80dbc67cd35d9772692b654ffc3af4c2807e
Nakyung Im <nakyoung99@gmail.com>
2021-01-05 14:47:17 +0900

    add redux-logger

--- noteapp-react-redux-antd/src/index.js
+++ noteapp-react-redux-antd/src/index.js
@@ -5,13 +5,19 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux"; // 수정
 import rootReducer from "./store/modules";
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(rootReducer, devTools);
-console.log(store.getState() + "스토어");
+import { createLogger } from "redux-logger"; // 추가
+
+const reduxLogger = createLogger(); // 추가
+
+// const devTools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+  
+const store = createStore(rootReducer, applyMiddleware(reduxLogger)); // 수정
+
+// console.log(store.getState() + "스토어");
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,7 +26,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
